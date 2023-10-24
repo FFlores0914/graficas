37141496aaf4b30a43de38ea80c7aa3ba979775c
FFlores0914 <fflores0914@outlook.com>
2023-10-23 19:29:09 -0600

    Se agrega la grafica limite de ejes

---
+++ limiteje.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+# Crear datos de ejemplo
+x = [1, 2, 3, 4, 5]
+y = [10, 5, 7, 8, 3]
+
+# Crear la gráfica
+plt.plot(x, y)
+
+# Personalizar etiquetas de los ejes
+plt.xlabel(0,6)
+plt.ylabel(0,8)
+
+# Mostrar la gráfica
+plt.show()
